a33a570111bcf43ef3f00dfbc3951ae455262287
Kinnell Shah <kinnell@gmail.com>
2018-09-12 22:46:39 -0400

    Updated GroupMe module with HTTPClient and API Base URL

--- lib/groupme.rb
+++ lib/groupme.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
+require 'httpclient'
+
 require 'groupme/configuration'
 require 'groupme/version'
 
 module GroupMe
+  API_BASE_URL = 'https://api.groupme.com/v3/'
+  CLIENT = HTTPClient.new(base_url: API_BASE_URL, default_header: { 'Content-Type' => 'application/json' })
+
   class << self
     attr_accessor :configuration
   end
