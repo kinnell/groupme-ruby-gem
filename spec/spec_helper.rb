6ea337efe6096ef7fb4ac12cfd976e63835732ff
Kinnell Shah <kinnell@gmail.com>
2018-09-12 22:04:09 -0400

    Installed WebMock for stubbing HTTP requests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,14 +2,18 @@
 
 require 'simplecov'
 require 'coveralls'
+require 'webmock'
 
 SimpleCov.start
 Coveralls.wear!
+WebMock.enable!
 
 require 'bundler/setup'
 require 'groupme'
 
 RSpec.configure do |config|
+  include WebMock::API
+
   config.example_status_persistence_file_path = '.rspec_status'
   config.disable_monkey_patching!
 
