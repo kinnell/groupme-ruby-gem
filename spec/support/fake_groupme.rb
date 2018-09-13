d49bf55215192ad5cfbc904e8ad4ab44309ac272
Kinnell Shah <kinnell@gmail.com>
2018-09-12 22:12:52 -0400

    Added method to get JSON response from a fixture json file

--- spec/support/fake_groupme.rb
+++ spec/support/fake_groupme.rb
@@ -3,4 +3,9 @@
 require 'sinatra/base'
 
 class FakeGroupMe < Sinatra::Base
+  private
+
+  def json_response(fixture_path)
+    File.open("#{File.dirname(__dir__)}/fixtures/#{fixture_path}.json").read
+  end
 end
