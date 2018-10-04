74cc9f97fce5aa22767ef284f81bddbf48ef86ef
Kinnell Shah <kinnell@gmail.com>
2018-10-04 02:32:07 -0400

    Refactored spec format with contexts for improved clarity

--- spec/groupme/configuration_spec.rb
+++ spec/groupme/configuration_spec.rb
@@ -3,14 +3,19 @@
 RSpec.describe GroupMe::Configuration do
   after(:each) { GroupMe.reset! }
 
-  it 'should return nil if an access_token is not set' do
-    expect(GroupMe.configuration.access_token).to eq(nil)
+  context 'when access token is not set' do
+    it 'should return nil' do
+      expect(GroupMe.configuration.access_token).to eq(nil)
+    end
   end
 
-  it 'should be able to set and retrieve a configured access token' do
-    sample_access_token = 'R3EypXu5HbiA1Gcq4RpBIV7ws8kxmwb1pFpCqHxY'
-    GroupMe.configure { |config| config.access_token = sample_access_token }
+  context 'when access token is set' do
+    let(:sample_access_token) { 'R3EypXu5HbiA1Gcq4RpBIV7ws8kxmwb1pFpCqHxY' }
 
-    expect(GroupMe.configuration.access_token).to eq(sample_access_token)
+    it 'should be able to set and retrieve it' do
+      GroupMe.configure { |config| config.access_token = sample_access_token }
+
+      expect(GroupMe.configuration.access_token).to eq(sample_access_token)
+    end
   end
 end
