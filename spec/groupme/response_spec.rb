9b3474c41ae654da7c6d2d8d2853f8f41b7a18a6
Kinnell Shah <kinnell@gmail.com>
2018-10-05 01:57:16 -0400

    Added spec for initializing GroupMe::Response object

--- spec/groupme/response_spec.rb
+++ spec/groupme/response_spec.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 RSpec.describe GroupMe::Response do
+  let(:request)  { GroupMe::Request.new(:get, 'groups') }
+  let(:response) { request.send! }
+
+  describe '#new' do
+    it 'should initialize a GroupMe::Response object' do
+      expect(response).to be_an_instance_of(GroupMe::Response)
+    end
+  end
 end
