0759c8665ec7a219c2f7e46d04e0d32863085b05
Kinnell Shah <kinnell@gmail.com>
2018-10-05 02:35:39 -0400

    Updated FakeGroupMe json response methods to better mimic actual responses

--- spec/support/fake_groupme.rb
+++ spec/support/fake_groupme.rb
@@ -10,7 +10,11 @@ class FakeGroupMe < Sinatra::Base
   end
   private
 
-  def json_response(fixture_path)
+  def json_response(fixture_path, status_code = 200)
+    "{\"response\":#{json_response_body(fixture_path)},\"meta\":{\"code\":#{status_code}}}"
+  end
+
+  def json_response_body(fixture_path)
     File.open("#{File.dirname(__dir__)}/fixtures/#{fixture_path}.json").read
   end
 end
