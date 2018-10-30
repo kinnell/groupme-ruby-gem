16d41c1ffc1d4ff5516132dddb020cff0c10a53a
Kinnell Shah <kinnell@gmail.com>
2018-10-30 07:46:18 -0400

    Add temporary way to use test account access_token if set

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,9 +14,6 @@ require 'groupme'
 require 'securerandom'
 
 RSpec.configure do |config|
-  # include WebMock::API
-  # WebMock.allow_net_connect!
-
   config.filter_run_when_matching :focus
 
   config.expect_with :rspec do |c|
@@ -28,5 +25,18 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures'
   config.hook_into :webmock
   config.allow_http_connections_when_no_cassette = true
-  config.filter_sensitive_data('<ACCESS_TOKEN>') { ACCESS_TOKEN }
+  config.filter_sensitive_data('<ACCESS_TOKEN>') { GroupMe.configuration.access_token || '' }
 end
+
+RSpec.shared_context :groupme_configured_with_access_token do
+  let(:fake_access_token) { SecureRandom.base64(30) }
+
+  before do
+    GroupMe.configure do |config|
+      config.access_token = defined?(TEST_ACCOUNT_ACCESS_TOKEN) ? TEST_ACCOUNT_ACCESS_TOKEN : fake_access_token
+    end
+  end
+end
+
+# Set Test Account's Access Token to generate fixtures (Not needed if fixtures generated)
+# TEST_ACCOUNT_ACCESS_TOKEN = ''
