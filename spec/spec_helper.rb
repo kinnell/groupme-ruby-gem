38b73e77c3587dd0b868405fca7fe7ac2d865a8d
Kinnell Shah <kinnell@gmail.com>
2018-10-30 06:57:23 -0400

    Update VCR cassette library directory

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,7 +25,7 @@ RSpec.configure do |config|
 end
 
 VCR.configure do |config|
-  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.cassette_library_dir = 'spec/fixtures'
   config.hook_into :webmock
   config.allow_http_connections_when_no_cassette = true
   config.filter_sensitive_data('<ACCESS_TOKEN>') { ACCESS_TOKEN }
