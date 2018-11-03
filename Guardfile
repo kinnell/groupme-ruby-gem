663251c792cd1a0bf179d3b882c2cc1c76f5ea48
Kinnell Shah <kinnell@gmail.com>
2018-11-03 03:50:48 -0400

    Add and configure VCR for recorded responses

--- Guardfile
+++ Guardfile
@@ -4,6 +4,5 @@ guard :rspec, cmd: 'rspec' do
   watch(%r{^spec/.+_spec\.rb$})
   watch(%r{^lib/(.+)\.rb$}) { |m| "spec/#{m[1]}_spec.rb" }
   watch('spec/spec_helper.rb') { 'spec' }
-  watch(%r{^spec/fixtures/(.+)\.json$}) { 'spec' }
   watch(%r{^spec/support/(.+)$}) { 'spec' }
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@
 require 'simplecov'
 require 'coveralls'
 require 'webmock'
+require 'vcr'
 
 SimpleCov.start
 Coveralls.wear!
@@ -13,12 +14,19 @@ require 'groupme'
 require 'securerandom'
 
 RSpec.configure do |config|
-  include WebMock::API
+  # include WebMock::API
+  # WebMock.allow_net_connect!
 
   config.filter_run_when_matching :focus
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+end
 
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.allow_http_connections_when_no_cassette = true
+  config.filter_sensitive_data('<ACCESS_TOKEN>') { ACCESS_TOKEN }
 end
