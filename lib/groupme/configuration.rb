f146b02116af35425bb593dfe809c6c9bf9eca9a
Kinnell Shah <kinnell@gmail.com>
2018-11-03 04:09:40 -0400

    Move Configuration related methods on main GroupMe file to Configuration file

--- lib/groupme/configuration.rb
+++ lib/groupme/configuration.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
 module GroupMe
+  class << self
+    def configuration
+      @configuration ||= Configuration.new
+    end
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  def self.reset!
+    @configuration = Configuration.new
+  end
+
   class Configuration
     attr_accessor :access_token
   end
