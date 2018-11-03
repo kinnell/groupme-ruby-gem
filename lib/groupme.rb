08c4f2d402c11d5fde9ac0231986d7824b5987eb
Kinnell Shah <kinnell@gmail.com>
2018-11-03 04:09:40 -0400

    Move Configuration related methods on main GroupMe file to Configuration file

--- lib/groupme.rb
+++ lib/groupme.rb
@@ -9,18 +9,4 @@ require 'groupme/request'
 require 'groupme/version'
 
 module GroupMe
-  class << self
-    def configuration
-      @configuration ||= Configuration.new
-    end
-  end
-
-  def self.configure
-    self.configuration ||= Configuration.new
-    yield(configuration)
-  end
-
-  def self.reset!
-    @configuration = Configuration.new
-  end
 end
