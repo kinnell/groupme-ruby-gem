8a2c410a77b717aaf1ea674c06d337a02436801f
Kinnell Shah <kinnell@gmail.com>
2018-11-05 02:19:04 -0500

    Refactor webmock request specs

--- spec/groupme/client_spec.rb
+++ spec/groupme/client_spec.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 RSpec.describe GroupMe::Client do
+  let(:client) { GroupMe::Client.new(access_token: ACCESS_TOKEN) }
+  let(:base_url) { 'https://api.groupme.com/v3/' }
+
   describe '.new' do
     context 'when :access_token argument is missing' do
       it 'should raise an error' do
@@ -16,32 +19,35 @@ RSpec.describe GroupMe::Client do
   end
 
   describe '#request' do
-    let(:client) { GroupMe::Client.new(access_token: ACCESS_TOKEN) }
-    let(:base_url) { 'https://api.groupme.com/v3/' }
+    let(:sample_data) { { id: 1, name: 'Group' } }
+    let(:response_body) { { response: sample_data }.to_json }
+    let(:response_status) { 200 }
 
     before do
-      stub_request(:get, 'groups')
-      stub_request(:post, 'groups')
+      stub_request(:any, /sample-path/).to_return(
+        body: response_body,
+        status: response_status
+      )
     end
 
     context 'when there are no parameters' do
-      it 'should work' do
+      it 'should send the correct HTTP request' do
         client.request(:get, 'groups')
 
-        expect(WebMock).to have_requested(:get, "#{base_url}groups?token=#{ACCESS_TOKEN}")
+        expect(WebMock).to have_requested(:get, "#{base_url}groups").with(query: { token: ACCESS_TOKEN })
       end
     end
 
     context 'when there are query parameters' do
-      it 'should work' do
-        client.request(:get, 'groups', query: { per_page: 100 })
+      it 'should send the correct HTTP request' do
+        client.request(:get, 'groups', query: { per_page: 1 })
 
-        expect(WebMock).to have_requested(:get, "#{base_url}groups?token=#{ACCESS_TOKEN}&per_page=100")
+        expect(WebMock).to have_requested(:get, "#{base_url}groups").with(query: { token: ACCESS_TOKEN, per_page: 1 })
       end
     end
 
     context 'when there are body parameters' do
-      it 'should work' do
+      it 'should send the correct HTTP request' do
         client.request(:post, 'groups', body: { name: 'Group' })
 
         expect(WebMock).to have_requested(:post, "#{base_url}groups?token=#{ACCESS_TOKEN}").with(body: 'name=Group')
