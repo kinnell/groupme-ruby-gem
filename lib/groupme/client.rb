c50ae05e88804875aae7c18a3fd745dcddbc6eab
Kinnell Shah <kinnell@gmail.com>
2018-11-05 00:08:23 -0500

    Update client class to be more readable

--- lib/groupme/client.rb
+++ lib/groupme/client.rb
@@ -3,13 +3,11 @@
 module GroupMe
   class Client
     API_BASE_URL = 'https://api.groupme.com/v3/'
+    API_DEFAULT_HEADER = { 'Content-Type': 'application/json' }.freeze
 
     def initialize(access_token:)
       @access_token = access_token
-      @client = HTTPClient.new do |config|
-        config.base_url = API_BASE_URL
-        config.default_header = { 'Content-Type': 'application/json' }
-      end
+      @client = HTTPClient.new(base_url: API_BASE_URL, default_header: API_DEFAULT_HEADER)
     end
 
     def request(method, path, query: {}, body: {})
