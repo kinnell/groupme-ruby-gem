b4d6f1a60d348b3c969ee2ccd95cb02a42ee1b02
Kinnell Shah <kinnell@gmail.com>
2018-11-05 03:43:30 -0500

    Add instance methods for #get, #post, #delete

--- lib/groupme/client.rb
+++ lib/groupme/client.rb
@@ -14,5 +14,17 @@ module GroupMe
       response = @client.request(method, path, { token: @access_token }.merge(query), body)
       JSON.parse(response.body, symbolize_names: true).fetch(:response) if response.ok?
     end
+
+    def get(path, query = {})
+      request(:get, path, query: query)
+    end
+
+    def post(path, body = {})
+      request(:post, path, body: body)
+    end
+
+    def delete(path, query = {})
+      request(:delete, path, query: query)
+    end
   end
 end
