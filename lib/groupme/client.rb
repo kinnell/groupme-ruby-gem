e902c220da19b539faf36482662498f9cc60dc29
Kinnell Shah <kinnell@gmail.com>
2018-11-10 16:29:38 -0500

    Update GroupMe::Client to use configuration

--- lib/groupme/client.rb
+++ lib/groupme/client.rb
@@ -5,8 +5,10 @@ module GroupMe
     API_BASE_URL = 'https://api.groupme.com/v3/'
     API_DEFAULT_HEADER = { 'Content-Type': 'application/json' }.freeze
 
-    def initialize(access_token:)
-      @access_token = access_token
+    attr_accessor :access_token
+
+    def initialize(args = {})
+      @access_token = args[:access_token] || GroupMe.configuration.access_token
       @client = HTTPClient.new(base_url: API_BASE_URL, default_header: API_DEFAULT_HEADER)
     end
 
