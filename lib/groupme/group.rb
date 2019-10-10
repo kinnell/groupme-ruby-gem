831d28adef13a5ea29ca1f2c04e392b31f882b3d
Kinnell Shah <kinnell@gmail.com>
2019-10-10 02:34:20 -0400

    Add initial Group endpoint

--- lib/groupme/group.rb
+++ lib/groupme/group.rb
@@ -2,5 +2,8 @@
 
 module GroupMe
   class Group
+    def self.all
+      GroupMe.client.get('groups', omit: :memberships, per_page: GroupMe.configuration.groups_per_page).first
+    end
   end
 end
--- spec/groupme/group_spec.rb
+++ spec/groupme/group_spec.rb
@@ -2,4 +2,14 @@
 
 RSpec.describe GroupMe::Group do
   include_context :with_default_groupme_configuration
+
+  let(:base_url) { GroupMe::Client::API_BASE_URL }
+
+  describe '#all' do
+    it 'should send the correct HTTP request' do
+      _groups = GroupMe::Group.all
+
+      expect(WebMock).to have_requested(:get, "#{base_url}groups").with(query: { token: access_token, omit: :memberships, per_page: GroupMe.configuration.groups_per_page })
+    end
+  end
 end
