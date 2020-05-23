ace069df1249a00d4b11241306e732d6c8931072
Kinnell Shah <kinnell@gmail.com>
2020-05-23 13:43:18 -0400

    Update groupme_spec.rb

--- spec/groupme_spec.rb
+++ spec/groupme_spec.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
-
 RSpec.describe GroupMe do
   it 'has a version number' do
-    expect(GroupMe::VERSION).not_to be nil
+    expect(GroupMe::VERSION).not_to be(nil)
   end
 end
